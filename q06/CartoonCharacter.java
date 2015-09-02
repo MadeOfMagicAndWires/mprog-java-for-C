49ca332fc492faf63dae09fa77d7368628e52f28
Joost Bremmer <toost.b@gmail.com>
2015-09-03 01:15:29 +0200

    q06: Completed CartoonCharacter.java and CartoonTest.java
    Complete functionality for CartoonTest.java and CartoonCharacter.java

--- q06/CartoonCharacter.java
+++ q06/CartoonCharacter.java
@@ -2,21 +2,31 @@ class CartoonCharacter {
 
   // Properties of the class...
   private String name;
-  private String favouriteColour;
-  private int    favouriteNumber;
+  //Q6 Set favouriteColour to public
+	public String  favouriteColour;
+	private int    favouriteNumber;
+	//Q7 Create new static int count
+	public static int count;
+	//Q8 remove static from int count
+	//public int count;
+
 
   // Constructor of the class...
   public CartoonCharacter(String aName, String aColour, int aNumber) {
-	name            = aName;
-	favouriteColour = aColour;
-	favouriteNumber = aNumber;
+		name            = aName;
+		favouriteColour = aColour;
+		favouriteNumber = aNumber;
+		//Increment static count int
+		count++;
   }
 
   // Methods of the class...
   public void displayMe() {
-	System.out.println("Hello, my name is " + name);
-	System.out.println("my favourite colour is " + favouriteColour);
-	System.out.println("and my favourite number is " + favouriteNumber);
+		System.out.println("Hello, my name is " + name);
+		System.out.println("my favourite colour is " + favouriteColour);
+		System.out.println("and my favourite number is " + favouriteNumber);
+		//Add newline
+		System.out.println();
   }
 }
 //vim: set tabstop=2 shiftwidth=2 expandtab:
--- q06/CartoonTest.java
+++ q06/CartoonTest.java
@@ -7,9 +7,9 @@ class FredFlintstone {
 
   // Methods of the class...
   static void displayMe() {
-	System.out.println("Hello, my name is " + name);
-	System.out.println("my favourite colour is " + favouriteColour);
-	System.out.println("and my favourite number is " + favouriteNumber);
+    System.out.println("Hello, my name is " + name);
+    System.out.println("my favourite colour is " + favouriteColour);
+    System.out.println("and my favourite number is " + favouriteNumber);
   }
 }
 
@@ -22,9 +22,9 @@ class WilmaFlintstone {
 
   // Methods of the class...
   static void displayMe() {
-	System.out.println("Hello, my name is " + name);
-	System.out.println("my favourite colour is " + favouriteColour);
-	System.out.println("and my favourite number is " + favouriteNumber);
+    System.out.println("Hello, my name is " + name);
+    System.out.println("my favourite colour is " + favouriteColour);
+    System.out.println("and my favourite number is " + favouriteNumber);
   }
 }
 
@@ -37,9 +37,9 @@ class BarneyRubble {
 
   // Methods of the class...
   static void displayMe() {
-	System.out.println("Hello, my name is " + name);
-	System.out.println("my favourite colour is " + favouriteColour);
-	System.out.println("and my favourite number is " + favouriteNumber);
+    System.out.println("Hello, my name is " + name);
+    System.out.println("my favourite colour is " + favouriteColour);
+    System.out.println("and my favourite number is " + favouriteNumber);
   }
 }
 
@@ -47,6 +47,33 @@ class CartoonTest {
 
   // The main method is the point of entry into the program...
   public static void main(String[] args) {
+
+    //Q2 Call Fred Flintstones displayMe method();
+    //FredFlintstone.displayMe();
+
+    //Q3 Print out Barney's favourite number without displayMe()
+    //System.out.println(BarneyRubble.favouriteNumber);
+
+    //Q4 Create three CartoonCharacter's called Fred, Wilma and Barney
+    CartoonCharacter b = new CartoonCharacter("Barney Rubble", "yellow",  2);
+    CartoonCharacter f = new CartoonCharacter("Fred Flintstone", "blue", 42);
+    CartoonCharacter w = new CartoonCharacter("Wilma Flintstone", "red", 63);
+
+    //Q5 call displayMe() on all new CartoonCharacters
+    b.displayMe();
+    f.displayMe();
+    w.displayMe();
+
+    //Q6 print Barney's favourite colour
+    System.out.println(b.favouriteColour);
+
+    //Q7 print amount of CartoonCharacter Objects created.
+    System.out.println();
+    System.out.println("The amount of CartoonCharacters created: " +
+                       CartoonCharacter.count);
+
+    //Q8 print out pi to stdout
+    System.out.println("the value of PI is: " + Math.PI);
   }
 }
 
