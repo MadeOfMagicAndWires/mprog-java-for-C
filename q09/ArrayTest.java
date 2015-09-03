72cf0454444500bc376149cdacf366fe69b40935
Joost Bremmer <toost.b@gmail.com>
2015-09-03 19:24:20 +0200

    q09: Complete ArrayTest.java and ArrayTest2.java; add ArrayPrint.class
    * Add ArrayPrint.class to cache and whitelist it in the.gitignore
    * Complete q09: 1(a-f), complete functionality for ArrayTest.java
    * Complete q09: 2(a-f), complete functionality for ArrayTest2.java

--- q09/ArrayTest.java
+++ q09/ArrayTest.java
@@ -1,27 +1,63 @@
 class ArrayTest {
 
   public static void main(String[] args) {
-	ArrayTest x = new ArrayTest();
-	x.doStuff();
+    ArrayTest x = new ArrayTest();
+    x.doStuff();
   }
 
   public void doStuff() {
 
-	// creates the array fred of ten integers.
-	int[] fred = new int[10];
+    // creates the array fred of ten integers.
+    int[] fred = new int[10];
 
-	// sets the values of the fred array
-	for (int i=0; i<10; i++) {
-    fred[i] = i;
-	}
+    // sets the values of the fred array
+    for (int i=0; i<10; i++) {
+      fred[i] = i;
+    }
 
-	// prints the values of the fred array
-	for (int i=0; i<10; i++) {
-    System.out.println(fred[i]);
-	}
+    // prints the values of the fred array
+    for (int i=0; i<10; i++) {
+      System.out.println(fred[i]);
+    }
+    System.out.println();
 
-	// insert your code for the nums array here:
+    //1b) Create double[] nums and c) initialize it.
+    double[] nums = new double[10];
+    for (int i=0; i < nums.length; i++){
+      nums[i] = 1 + (i/10.0);
+    }
+
+    //1d) printout array
+    System.out.println("Printing out nums[]");
+    for(int i=0;i<nums.length;i++)
+    {
+      System.out.println(nums[i]);
+    }
+    System.out.println();
+
+    //1e) print out array using ArrayPrint.printArray();
+    System.out.println("Printing out nums[] using ArrayPrint.printArray()");
+    ArrayPrint.printArray(nums);
+    System.out.println();
+
+    //1f) print out array using ArrayTest.printArray()
+    System.out.println("Printing out nums[] using ArrayTest.printArray()");
+    printArray(nums);
 
   }
+
+  //1f) create own printArray method()
+  static public void printArray(double[] array) {
+  /**
+   * Print out each member of an array of doubles
+   * @param array an array of doubles
+   */
+    for(int i=0;i<array.length;i++)
+    {
+      System.out.println(array[i]);
+    }
+    System.out.println();
+  }
+
 }
 //vim: set tabstop=2 shiftwidth=2 expandtab:
--- q09/ArrayTest2.java
+++ q09/ArrayTest2.java
@@ -17,14 +17,45 @@ class Human {
 class ArrayTest2 {
 
   public static void main(String[] args) {
-	ArrayTest2 x = new ArrayTest2();
-	x.doStuff();
+    ArrayTest2 x = new ArrayTest2();
+    x.doStuff();
   }
 
   public void doStuff() {
 
-	// insert your code here:
+    //2b) Create array threesome[]
+    Human threesome[] = new Human[3];
 
+    //2c) Initialize the threesome
+    threesome[0] = new Human("Angus");
+    threesome[1] = new Human("Brian");
+    threesome[2] = new Human("Charles");
+
+    //2d) print out each Human's name
+    System.out.println("Printing out threesome[]:");
+    for(int i=0;i<threesome.length;i++) {
+      System.out.println(threesome[i].toString());
+    }
+    System.out.println();
+
+    //2f) print out each Human's name using ArrayTest2.printArray()
+    System.out.println("Printing out threesome[]" +
+                       "using ArrayTest2.printArray():");
+    printArray(threesome);
+
+  }
+
+  //2e) Create a printArray method for Humans
+  static public void printArray(Human[] array) {
+  /**
+   * Print out each item from an array of Humans
+   * @param array an array of Human Objects
+   */
+
+    for(int i=0; i < array.length; i++) {
+      System.out.println(array[i].toString());
+    }
+    System.out.println();
   }
 }
 //vim: set tabstop=2 shiftwidth=2 expandtab:
