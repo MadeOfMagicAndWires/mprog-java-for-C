ee3344c416e616467f5cf608900ead2bf45b5164
Joost Bremmer <toost.b@gmail.com>
2015-09-03 18:27:35 +0200

    q08: Completed LoopTest.java
    Full functionality for LoopTest.java

--- q08/LoopTest.java
+++ q08/LoopTest.java
@@ -6,43 +6,102 @@ public class LoopTest {
   }
   // add your code here...
   public void doStuff() {
+  /** Test the various powerOf2*() methods of the LoopTest class */
+
+    //Q2 Initial Test powerOf2*() methods.
+    //Result should be 16.
+    testPowerOf2(4);
+
+    //Q3 Test PowerOf2*() with int 0;
+    testPowerOf2(0);
+
+    //Q4 Test PrintLine*() methods
+    testPrintLine(4);
+    testPrintLine(0);
+    testPrintLine(-1);
 
   }
+
+  public void testPowerOf2(int n) {
+  /** Tests all the testPowerOf2*() methods at once */
+    System.out.println("powerOf2A(" + n + "): " + powerOf2A(n));
+    System.out.println("powerOf2B(" + n + "): " + powerOf2B(n));
+    System.out.println("powerOf2C(" + n + "): " + powerOf2C(n));
+    System.out.println();
+  }
+
+  public void testPrintLine(int length) {
+  /** Tests all the printLine*() methods at once */
+    System.out.println("Test printLine*(" + length +  ")");
+    printLineA(length);
+    printLineB(length);
+    printLineC(length);
+    System.out.println();
+  }
+
   // These functions compute powers of two.
   public int powerOf2A(int n) {
-	int counter = n;
-	int result  = 1;
-	while (counter != 0) {
-    result = 2 * result;
-	  counter--;
-	}
-	return result;
+    int counter = n;
+    int result  = 1;
+    while (counter != 0) {
+      result = 2 * result;
+      counter--;
+    }
+    return result;
   }
+
   public int powerOf2B(int n) {
-	int counter = n;
-	int result  = 1;
-	do {
-    result = 2 * result;
-    counter--;
-	} while (counter != 0);
-	return result;
+    if (n == 0)
+      return 1;
+
+    int counter = n;
+    int result  = 1;
+    do {
+      result = 2 * result;
+      counter--;
+    } while (counter >= 0);
+    return result;
   }
+
   public int powerOf2C(int n) {
-	int counter;
-	int result;
-	for (counter = n, result = 1; counter != 0; counter--) {
-    result = 2 * result;
-	}
-	return result;
+    int counter;
+    int result;
+    for (counter = n, result = 1; counter != 0; counter--) {
+      result = 2 * result;
+    }
+    return result;
   }
-  /**
-   *  Prints a row of stars of a given length.
-   */
+
+  public void printLineA(int length) {
+ /** Prints a row of crunches of a given length. */
+    int i = length;
+
+    while (i > 0) {
+      System.out.print("#");
+      i--;
+    }
+    System.out.println();
+  }
+
+  public void printLineB(int length) {
+  /** Prints a row of crunches of a given length. */
+
+    int i = length;
+    do {
+      if (i <= 0)
+        break;
+      System.out.print("#");
+      i--;
+    } while (i > 0);
+    System.out.println();
+  }
+
   public void printLineC(int length) {
-	for (int i=0; i<length; i++) {
-    System.out.print("#");
-	}
-	System.out.println();
+  /** Prints a row of crunches of a given length. */
+    for (int i=0; i<length; i++) {
+      System.out.print("#");
+    }
+    System.out.println();
   }
 }
 //vim:set tabstop=2 shiftwidth=2 expandtab:
