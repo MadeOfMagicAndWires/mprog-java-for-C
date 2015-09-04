533eec2df23b5c393f9ab4467cb1c5923475ff7a
Joost Bremmer <toost.b@gmail.com>
2015-09-04 15:40:05 +0200

    q11: Complete AnimalTest.java and StarWars.java
    AnimalTest.java: Full functionality according to 1(a-f)
    StarWars.java  : Full functionality according to 2(a-f)

--- q11/AnimalTest.java
+++ q11/AnimalTest.java
@@ -6,13 +6,13 @@ class Animal {
 
   // Constructor of the class...
   public Animal() {
-	numberOfLegs = 4;
-	hasWings = false;
+    numberOfLegs = 4;
+    hasWings = false;
   }
 
   // Methods of the class...
   public void talk() {
-	System.out.println("Hello");
+    System.out.println("Hello");
   }
 }
 
@@ -23,14 +23,14 @@ class Bird extends Animal {
 
   // Constructor of the class...
   public Bird() {
-	numberOfLegs = 2;
-	hasWings = true;
-	canFly = true;
+    numberOfLegs = 2;
+    hasWings = true;
+    canFly = true;
   }
 
   // Methods of the class...
   public void fly() {
-	System.out.println("flap flap");
+    System.out.println("flap flap");
   }
 }
 
@@ -41,12 +41,12 @@ class Eagle extends Bird {
 
   // Constructor of the class...
   Eagle() {
-	numberOfKills = 0;
+    numberOfKills = 0;
   }
 
   // Methods of the class...
   public void attack() {
-	numberOfKills++;
+    numberOfKills++;
   }
 }
 
@@ -55,27 +55,47 @@ class AnimalTest {
   // The main method is the point of entry into the program...
   public static void main(String[] args) {
 
-	Animal a = new Animal();
-	System.out.println(a.numberOfLegs);
-	System.out.println(a.hasWings);
-	a.talk();
-	a.fly();
+    Animal a = new Animal();
+    System.out.println(a.numberOfLegs);
+    System.out.println(a.hasWings);
+    a.talk();
+    //1b) Animal class doesn't have fly() method
+    //a.fly();
 
-	Bird b = new Bird();
-	System.out.println(b.numberOfLegs);
-	System.out.println(b.hasWings);
-	System.out.println(b.canFly);
-	System.out.println(b.numberOfKills);
-	b.talk();
-  b.attack();
+    Bird b = new Bird();
+    System.out.println(b.numberOfLegs);
+    System.out.println(b.hasWings);
+    System.out.println(b.canFly);
+    //1b) Bird class doesn't have numberOfKills property
+    //System.out.println(b.numberOfKills);
+    b.talk();
+    //1b) Bird class doesn't have attack() method
+    //b.attack();
 
-	Eagle e = new Eagle();
-	System.out.println(e.numberOfKills);
-	System.out.println(e.numberOfLegs);
-	System.out.println(e.hasWings);
-	e.talk();
-	e.attack();
+    //1d) numberOfLegs=2 thanks to the inherreted Bird constructor
+    Eagle e = new Eagle();
+    //1b) numberOfKills is set to private
+    //System.out.println(e.numberOfKills);
+    System.out.println(e.numberOfLegs);
+    System.out.println(e.hasWings);
+    e.talk();
+    e.attack();
+    System.out.println();
 
+    //1e) change a to b; try to fly
+    a = b;
+    a.talk();
+    //1e) still gives an error! the Bird object is recast as an Animal!
+    //a.fly();
+    System.out.println();
+
+    //1f) change b to a; try to fly
+    //But you an Animal object is not recast as a Bird!
+    //In short, there's enough data to revert to a superclass,
+    //But you can't "evolve" into a subclass without the missing properties.
+    //b = a;
+    b.talk();
+    b.fly();
   }
 }
 //vim: set tabstop=2 shiftwidth=2 expandtab:
--- q11/StarWars.java
+++ q11/StarWars.java
@@ -1,58 +1,105 @@
-class XWing {
+/**
+ * 2b) Spaceship superclass to fight the Star Wars with.
+ *
+ * <p>
+ * This class is used as an representation of a generic spaceship
+ * It has shield and weapon capabilities with which it can shoot and get hit.
+ *
+ * It's shield capacity is represented by a protected int "shields".
+ * It's firing power is represented by a protected int "weapon".
+ * It's current status (dead/alive) is respresented by a boolean "dead".
+ * </p>
+ *
+ * @author  Joost Bremmer
+ * @version 1.0
+ * @since   2015
+ */
+abstract class SpaceShip {
 
-  // Properties of the class...
-  private int     shields;
-  private int     weapon;
-  private boolean dead;
+  //2c) Properties of the class...
+  protected int     shields;
+  protected int     weapon;
+  protected boolean dead;
 
-  // Constructor of the class...
-  public XWing() {
-	shields = 1000;
-	weapon  = 10;
+  //2d) Methods of the class...
+
+  /**
+   * Constructor for the SpaceShip class
+   * Sets weapon and shield capacity to a generic 100
+   * Sets current status to false
+   * @see SpaceShip
+   */
+  public SpaceShip() {
+    shields = 100;
+    weapon  = 100;
+    dead    = false;
   }
 
-  // Methods of the class...
+  /** Returns the weapon power of the SpaceShip as an int */
   public int getWeapon() {
-	return weapon;
+    return weapon;
   }
+
+  /** Returns whether the SpaceShip is dead or not */
   public boolean isDead() {
-	return dead;
-  }
-  public void hit(int damage) {
-	shields = shields - damage;
-	if (shields < 0) {
-    System.out.println("BOOM!!!");
-    dead = true;
-	}
-  }
-}
-
-class Tie {
-
-  // Properties of the class...
-  private int shields;
-  private int weapon;
-  private boolean dead;
-
-  // Constructor of the class...
-  public Tie() {
-	shields = 500;
-	weapon  = 20;
+    return dead;
   }
 
-  // Methods of the class...
-  public int getWeapon() {
-	return weapon;
-  }
-  public boolean isDead() {
-	return dead;
-  }
+  /**
+   * Deals damage to the spaceship;
+   * if it is hit with more damage than there's shield
+   * capacity left, the ship will die.
+   * @param damage amount of damage being dealt, as an int.
+   * */
   public void hit(int damage) {
     shields = shields - damage;
     if (shields < 0) {
       System.out.println("BOOM!!!");
       dead = true;
     }
+  }
+}
+//2e) extend subclasses
+/**
+ * X-Wing subclass of SpaceShip
+ *
+ * <p>
+ * It has better shields but less firepower
+ * than other ships.
+ */
+class XWing extends SpaceShip {
+
+  /**
+   * Constructor of the XWing subclass
+   * Sets shields to 1000
+   * Sets weapon  to 10
+   * @see SpaceShip#Constructor
+   */
+  public XWing() {
+    shields = 1000;
+    weapon  = 10;
+  }
+
+}
+//2e) extend subclasses
+/**
+ * Tie fighter subclass of SpaceShip
+ *
+ * <p>
+ * It has subpar shields but better firepower
+ * than other ships.
+ */
+class Tie extends SpaceShip {
+
+  /**
+   * Constructor of the TieFighter subclass
+   * Sets shields to 500
+   * Sets weapon  to 20
+   * @see SpaceShip#Constructor
+   */
+   public Tie() {
+    shields = 500;
+    weapon  = 20;
   }
 }
 
@@ -66,84 +113,87 @@ class StarWars {
         System.out.println("X-Wing is dead");
         break;
       }
-    t.hit(x.getWeapon());
-    if (t.isDead()) {
-      System.out.println("Tie fighter is dead");
-      break;
+      t.hit(x.getWeapon());
+      if (t.isDead()) {
+        System.out.println("Tie fighter is dead");
+        break;
+      }
     }
-  }
 
   }
 
   private void battle(XWing[] good, Tie[] evil) {
 
-	int g = 0;
-	int e = 0;
-	int goodDeaths = 0;
-	int evilDeaths = 0;
+    int g = 0;
+    int e = 0;
+    int goodDeaths = 0;
+    int evilDeaths = 0;
 
-	while (g < good.length && e < evil.length) {
-    System.out.println("battling X-Wing #" + g + " versus Tie Fighter #" + e);
-    duel(good[g],evil[e]);
-    if (good[g].isDead()) {
-      g++;
-      goodDeaths++;
+    while (g < good.length && e < evil.length) {
+      System.out.println("battling X-Wing #" + g + " versus Tie Fighter #" + e);
+      duel(good[g],evil[e]);
+      if (good[g].isDead()) {
+        g++;
+        goodDeaths++;
+      }
+
+      if (evil[e].isDead()) {
+        e++;
+        evilDeaths++;
+      }
     }
 
-    if (evil[e].isDead()) {
-      e++;
-      evilDeaths++;
+    int finalGood = good.length - goodDeaths;
+    int finalEvil = evil.length - evilDeaths;
+
+    System.out.println();
+    System.out.println("Battle Report:\t\tX-Wings\t\tTie Fighters");
+    System.out.println("------------------------------------------------------");
+    System.out.println();
+    System.out.println("Initial ships:\t\t" + good.length + "\t\t" + evil.length);
+    System.out.println();
+    System.out.println("Killed ships:\t\t"  + goodDeaths  + "\t\t" + evilDeaths);
+    System.out.println();
+    System.out.println("Final ships:\t\t"   + finalGood   + "\t\t" + finalEvil);
+    System.out.println();
+    if (finalGood > finalEvil) {
+      System.out.println("The rebel alliance is victorious!");
     }
-	}
-
-	int finalGood = good.length - goodDeaths;
-	int finalEvil = evil.length - evilDeaths;
-
-	System.out.println();
-	System.out.println("Battle Report:\t\tX-Wings\t\tTie Fighters");
-	System.out.println("------------------------------------------------------");
-	System.out.println();
-	System.out.println("Initial ships:\t\t" + good.length + "\t\t" + evil.length);
-	System.out.println();
-	System.out.println("Killed ships:\t\t"  + goodDeaths  + "\t\t" + evilDeaths);
-	System.out.println();
-	System.out.println("Final ships:\t\t"   + finalGood   + "\t\t" + finalEvil);
-	System.out.println();
-	if (finalGood > finalEvil) {
-    System.out.println("The rebel alliance is victorious!");
-	}
-	else {
-    System.out.println("The dark side has conquered!");
-	}
-	System.out.println();
+    else {
+      System.out.println("The dark side has conquered!");
+    }
+    System.out.println();
   }
 
   public void doStuff() {
 
-	// defines the goodies array
-	XWing[] goodies = new XWing[3];
+    // defines the goodies array
+    XWing[] goodies = new XWing[3];
 
-	// initialises the elements of the goodies array
-	for (int i=0; i<goodies.length; i++) {
-    goodies[i] = new XWing();
-	}
+    // initialises the elements of the goodies array
+    for (int i=0; i<goodies.length; i++) {
+      goodies[i] = new XWing();
+    }
 
-	// defines the baddies array
-	Tie[] baddies = new Tie[3];
+    // defines the baddies array
+    Tie[] baddies = new Tie[3];
 
-	// initialises the elements of the baddies array
-	for (int i=0; i<baddies.length; i++) {
-    baddies[i] = new Tie();
-	}
+    // initialises the elements of the baddies array
+    for (int i=0; i<baddies.length; i++) {
+      baddies[i] = new Tie();
+    }
 
-	battle(goodies,baddies);
+    battle(goodies,baddies);
+
+    //2g) Try to make a new instance of an abstract class
+    //Spaceship milleniumFalcon = new SpaceShip();
 
   }
 
   // The main method is the point of entry into the program...
   public static void main(String[] args) {
-	StarWars me = new StarWars();
-	me.doStuff();
+    StarWars me = new StarWars();
+    me.doStuff();
   }
 
 }
